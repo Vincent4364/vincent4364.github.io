b595dac97fa148557f01f6e8749244f10ab43df3
Vincent Gregorash <21060@wc.school.nz>
2025-08-13 09:28:10 +1200

    6th commit - changed database

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
-DATABASE = 'Times_Table'
+DATABASE = 'times_table'
 
 def connect_to_database(db_file):
     """
@@ -26,7 +26,7 @@ def index():
 def submit():
     con = connect_to_database(DATABASE)
     cur = con.cursor()
-    time = request.form.get('time')
+    time = int(request.form.get('time'))
     if time:
         print(f"Received reaction time: {time} ms")  # Debug print
         cur.execute("INSERT INTO user_times VALUES (?)", (time,))
