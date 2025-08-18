3fa3c697589c218670ecc9f35d3da696db053f48
Vincent Gregorash <21060@wc.school.nz>
2025-08-19 11:44:09 +1200

    8th commit - updated database

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def connect_to_database(db_file):
 def index():
     return render_template('index.html')
 
-# New route to receive data from ESP32
 @app.route('/submit', methods=['POST'])
 def submit():
     con = connect_to_database(DATABASE)
@@ -37,5 +36,4 @@ def submit():
     return "Missing time", 400
 
 if __name__ == '__main__':
-    # host='0.0.0.0' makes Flask listen on your local network
     app.run(debug=True, host='0.0.0.0', port=5000)
